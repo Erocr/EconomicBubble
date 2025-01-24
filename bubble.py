b1b3de37e9cf27ff982bf267018b277da122fee0
Panda-d3v <104253721+Panda-d3v@users.noreply.github.com>
2025-01-24 23:32:27 +0100

    added a timer in bubble.py

--- bubble.py
+++ bubble.py
@@ -1,4 +1,5 @@
 from vec import Vec
+from inputs import Inputs
 
 
 class Bubble:
@@ -6,9 +7,11 @@ class Bubble:
     radius : float
     center : Vec
     fill_level : float between 0 and 1
+    timer : int ables the animation, like a frameCount
     """
 
     def __init__(self, radius: float, center: Vec, fill_level: float):
         self.radius = radius
         self.center = center
         self.fill_level = fill_level
+        self.timer = 0
