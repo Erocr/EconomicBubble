666bc535af9bc5809d92d28617ca7128a4a9f81b
Nonlinear2 <131959792+Nonlinear2@users.noreply.github.com>
2025-01-25 16:01:44 +0100

    update visuals

--- main.py
+++ main.py
@@ -9,12 +9,19 @@ visual_data = VisualData(view)
 economy_graph = EconomyGraph(visual_data)
 clock = pg.time.Clock()
 
+frames = 0
 while not inputs.quit:
     inputs.update()
 
     economy_graph.update_visuals(view, inputs)
+    
+    if (frames > 10):
+        economy_graph.update_simulation()
+        frames %= 10
+
     view.flip()
 
     clock.tick(50)
+    frames++;
 
 pg.quit()
