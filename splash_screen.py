8f6113bb7b02b104b1b3b5b512fc0cc3d409bdef
Niccolo Angeli <niccolo.angeli@universite-paris-saclay.fr>
2025-01-25 18:50:31 +0100

    Implémentation d'un splash screen final

--- splash_screen.py
+++ splash_screen.py
@@ -1,9 +1,10 @@
 import pygame as pg
+import sys
 
 
 class SplashScreen:
     def __init__(self, screenSize):
-        self.bg = pg.Surface(screenSize.get)
+        self.bg = pg.image.load(sys.path[0] + "/images/Splash_Screen.jpg")
 
     def draw(self, view):
         view.screen.blit(self.bg, (0, 0))
