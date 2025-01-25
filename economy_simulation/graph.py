b591e6aa450fb37733026cf263bac6e768103b7f
Nonlinear2 <131959792+Nonlinear2@users.noreply.github.com>
2025-01-25 14:13:27 +0100

    update simulation

--- economy_simulation/graph.py
+++ economy_simulation/graph.py
@@ -6,43 +6,36 @@ import random
 class EconomyGraph:
     def __init__(self):
         # Nodes
-        TC = core.TrueCapitalNode()     # True capital
-        AC = core.ApparentCapitalNode() # Apparent capital
-        SoapM = core.MarketNode()       # Soap Market
-        BeerM = core.MarketNode()       # Beer Market
-        OperaM = core.MarketNode()      # Soap-Opera Market
-        PD = core.PublicDoubtNode()     # Public Doubt
-        ID = core.InvestorsDoubtNode() # Investors Doubt
-        Events = core.EventNode()       # Events
+        self.TC = core.TrueCapitalNode()     # True capital
+        self.AC = core.ApparentCapitalNode() # Apparent capital
+        self.SoapM = core.MarketNode()       # Soap Market
+        self.BeerM = core.MarketNode()       # Beer Market
+        self.OperaM = core.MarketNode()      # Soap-Opera Market
+        self.PD = core.PublicDoubtNode()     # Public Doubt
+        self.ID = core.InvestorsDoubtNode() # Investors Doubt
+        self.Events = core.EventNode()       # Events
 
-        M1 = core.MarketingNode()       # Marketing 1 : press message
-        M2 = core.MarketingNode()       # Marketing 2 : ads
-        S1 = core.SecurityNode()        # Security 2 : media control
-        S2 = core.SecurityNode()        # Security 2 : surveillance
-        Spy = core.EspionageNode()       # Espionage
+        self.M1 = core.MarketingNode()       # Marketing 1 : press message
+        self.M2 = core.MarketingNode()       # Marketing 2 : ads
+        self.S1 = core.SecurityNode()        # Security 2 : media control
+        self.S2 = core.SecurityNode()        # Security 2 : surveillance
+        self.Spy = core.EspionageNode()       # Espionage
 
         self.nodes = [
-            TC, AC, SoapM, BeerM, OperaM, PD, ID, Events
+            self.TC, self.AC, self.SoapM, self.BeerM, self.OperaM, self.PD, self.ID, self.Events
         ]
         
         # Edges
-        TC.addParents([SoapM, BeerM, OperaM, ID])
-        AC.addParents([TC, M1, PD])
-        SoapM.addParents([PD])
-        BeerM.addParents([PD])
-        OperaM.addParents([PD])
-        PD.addParents([M2, Events])
-        ID.addParents([S1, Events, AC, ID])
-        M1.addParents([S1])
-        M2.addParents([S2])
-        Events.addParents([S2, Spy])
-        
-        # self.market_nodes = (core.MarketNode(),)*3
-        # self.nodes = [*self.market_nodes]
-
-        # self.influence_table = {
-            # 0: (2, 3, 4)
-        # }
+        self.TC.addParents([self.SoapM, self.BeerM, self.OperaM, self.ID])
+        self.AC.addParents([self.TC, self.M1, self.PD])
+        self.SoapM.addParents([self.PD])
+        self.BeerM.addParents([self.PD])
+        self.OperaM.addParents([self.PD])
+        self.PD.addParents([self.M2, self.Events])
+        self.ID.addParents([self.S1, self.Events, self.AC, self.ID])
+        self.M1.addParents([self.S1])
+        self.M2.addParents([self.S2])
+        self.Events.addParents([self.S2, self.Spy])
 
     def update(self):
         random.shuffle(self.nodes)
@@ -54,6 +47,23 @@ class EconomyGraph:
             print(node.__name__)
             print(node._value)
 
+# def main2():
+#     economy = EconomyGraph()
+#     time = np.arange(100)
+#     for i in time:
+#         economy.update()
+
+#     i, l = 0, len(economy.nodes)
+#     fig = plt.figure(figsize = [l // 5, 5])
+#     for node in economy.nodes:
+#         ax = plt.subplot(i // 5 + 1, i + 1, i + 1)
+#         ax.plot(time, node.value_history, label = str(node))
+    
+#         ax.legend()
+#         i += 1
+#     plt.show()
+
+
 def main():
     economy = EconomyGraph()
     time = np.arange(100)
@@ -62,7 +72,7 @@ def main():
 
     for node in economy.nodes:
         plt.plot(time, node.value_history, label = str(node))
-    
+
     plt.legend()
     plt.show()
 
