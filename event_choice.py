6c8cb69b7cc1593927cdc489a1567b2bed0e3c80
Niccolo Angeli <niccolo.angeli@universite-paris-saclay.fr>
2025-01-26 15:40:09 +0100

    cards

--- event_choice.py
+++ event_choice.py
@@ -6,8 +6,7 @@ import sys
 class Card:
     image = pg.image.load(sys.path[0] + "/images/card_bg.png")
 
-    def __init__(self, action, narration, index, view):
-        self.action = action
+    def __init__(self, narration, index, view):
         self.narration = narration
         self.size = Vec(300, 500)
         self.pos = view.screenSize/2+Vec(-self.size.x*(1-index), -self.size.y/2)
@@ -21,16 +20,20 @@ class Card:
         view.text(self.narration, self.pos + self.size / 2, self.size.x, (255, 255, 255))
         # button
         view.circle(self.b_pos, self.b_rad, (30, 30, 30), filled=True)
-        view.text_centered(self.action, self.b_pos, self.b_rad, (255, 255, 255))
 
     def update(self, inputs):
         return inputs.pressed("mouse_left") and dist(inputs.mouse_pos, self.pos) < self.b_rad
 
 
 class CardsPair:
-    def __init__(self, action1, narration1, action2, narration2, view):
-        self.card1 = Card(action1, narration1, 0, view)
-        self.card2 = Card(action2, narration2, 1, view)
+    def __init__(self):
+        self.card1 = None
+
+        self.card2 = None
+
+    def set_cards(self, action1, action2, view):
+        self.card1 = Card(action1, 0, view)
+        self.card2 = Card(action2, 1, view)
 
     def draw(self, view):
         self.card1.draw(view)
